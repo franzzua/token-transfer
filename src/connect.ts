8a3072d449cc70b1fbff3daf811dd267ff6e47ee
fransua <fransua.ne@gmail.com>
2023-11-02 13:23:28 +0100

    [wip] wallet connect bad

--- src/connect.ts
+++ src/connect.ts
@@ -1,5 +1,5 @@
-// import { WalletConnectModalSign } from "@walletconnect/modal-sign-html";
-// import { EthereumProvider } from "@walletconnect/ethereum-provider";
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5';
+
 const provider = window.ethereum;
 const button = document.getElementById('connect') as HTMLButtonElement;
 const loader = document.getElementById('loader');
@@ -14,14 +14,36 @@ const loader = document.getElementById('loader');
 //     },
 // });
 
-if (!provider){
-    button.disabled = true;
+const modal = createWeb3Modal({
+    ethersConfig: defaultConfig({
+        metadata: {
+            name: 'My Website',
+            description: 'My Website description',
+            url: 'https://mywebsite.com',
+            icons: ['https://avatars.mywebsite.com/']
+        }
+    }),
+    chains: [{
+        chainId: 1,
+        name: 'Ethereum',
+        currency: 'ETH',
+        explorerUrl: 'https://etherscan.io',
+        rpcUrl: 'https://cloudflare-eth.com'
+    }],
+    projectId: '4489459e56ec630d406a7cab1e3a3fec',
+})
 
-} else {
-    provider.addListener('accountsChanged', notifyEthereumState);
+// if (!provider){
+//     button.disabled = true;
+//
+// } else {
+    // provider.addListener('accountsChanged', notifyEthereumState);
     async function connect(){
         loader.style.display = 'initial';
         button.style.display = 'none';
+        await modal.open({
+            view: "Connect"
+        });
         // globalThis.ethereum = await EthereumProvider.init({
         //     projectId: '4489459e56ec630d406a7cab1e3a3fec',
         //     showQrModal: true,
@@ -46,9 +68,9 @@ if (!provider){
             // },
 
         // }).catch();
-        await provider.request<string[]>({ method: 'eth_requestAccounts' }).catch(() => {
+        // await provider.request<string[]>({ method: 'eth_requestAccounts' }).catch(() => {
             // rejected and ok
-        });
+        // });
         button.style.display = 'initial';
         loader.style.display = 'none';
     }
@@ -80,7 +102,7 @@ if (!provider){
     })
 
     loader.style.display = 'none';
-    provider.request({method: "eth_accounts"}).then(notifyEthereumState);
+    // provider.request({method: "eth_accounts"}).then(notifyEthereumState);
     button.style.display = 'initial';
     button.addEventListener('click', connect);
-}
+// }
